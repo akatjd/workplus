e976f08b246d7cc613a285b7871536d6bc8bcdc1
akatjd <akatjd11@gmail.com>
2021-05-29 12:58:31 +0900

    환경설정 - MySQL 연동까지 완료

---
+++ workplus/src/main/java/com/kjc/workplus/WorkplusApplication.java
@@ -0,0 +1,13 @@
+package com.kjc.workplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkplusApplication.class, args);
+	}
+
+}
---
+++ workplus/src/test/java/com/kjc/workplus/WorkplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjc.workplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
